0d93af86b3e1260d04cdeaec43b035ece2561737
Orlandroid <lopez.orlando.gold9@gmail.com>
2023-08-01 19:30:20 -0600

    Update gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,8 @@ buildscript {
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "7.4.2" apply false
-    id("com.android.library") version "7.4.2" apply false
+    id("com.android.application") version "8.1.0" apply false
+    id("com.android.library") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.20" apply false
     id("org.jetbrains.kotlin.jvm") version "1.8.20" apply false
     id("com.google.dagger.hilt.android") version "2.45" apply false
