d264503d5731c24857e82e0af5aad86a44c00e62
Orlandroid <lopez.orlando.gold9@gmail.com>
2023-09-13 22:27:46 -0600

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,16 +8,14 @@ buildscript {
     }
     dependencies {
         val navigationVersion = "2.5.3"
-        //val daggerHiltVersion = "2.45"
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navigationVersion")
-       // classpath("com.google.dagger:hilt-android-gradle-plugin:$daggerHiltVersion")
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("com.android.library") version "8.1.0" apply false
+    id("com.android.application") version "8.1.1" apply false
+    id("com.android.library") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.20" apply false
     id("org.jetbrains.kotlin.jvm") version "1.8.20" apply false
     id("com.google.dagger.hilt.android") version "2.45" apply false
